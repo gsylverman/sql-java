2f14243bbdbe9de9b4f69645339b9e19eede2b8c
gsylverman <gsylverman@gmail.com>
2020-10-08 14:29:09 +0300

    add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
@@ -10,6 +11,12 @@ public class Main {
         try {
             Class.forName("org.sqlite.JDBC");
             Connection connection = DriverManager.getConnection("jdbc:sqlite:E:\\Databases\\testjava.db");
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
+                    "(name TEXT, phone INTEGER, email TEXT)");
+
+            statement.close();
+            connection.close();
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Something went wrong " + e.getMessage());
         }

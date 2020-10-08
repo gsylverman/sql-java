400c3c1f66b356dfdd8df938a0ad7770d570109f
gsylverman <gsylverman@gmail.com>
2020-10-08 14:54:09 +0300

    add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,8 +12,14 @@ public class Main {
             Class.forName("org.sqlite.JDBC");
             Connection connection = DriverManager.getConnection("jdbc:sqlite:E:\\Databases\\testjava.db");
             Statement statement = connection.createStatement();
+//            connection.setAutoCommit(false);
             statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
                     "(name TEXT, phone INTEGER, email TEXT)");
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES('Jane', 1121221, 'jane@gmail.com')");
+//            statement.execute("UPDATE contacts SET phone=10123546 "+
+//                    "WHERE contacts.name='Jane'");
+//            statement.execute("DELETE FROM contacts WHERE name='Grigore'");
 
             statement.close();
             connection.close();
